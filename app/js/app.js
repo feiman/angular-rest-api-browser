253ba794896296dfd5d46757f082e018e16c29ec
Harold Martin <harold.martin@openx.com>
2014-03-31 23:10:52 -0700

    clean up seed project, bootstrap column browser

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,12 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('restBrowser', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'restBrowser.controllers',
+  'millerColumnBrowser'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
